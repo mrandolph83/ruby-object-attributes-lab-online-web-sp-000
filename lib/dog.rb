a3227038e8e43ce35d343c59ecbd172470b99675
Matt Smith <mrandolph83@gmail.com>
2020-10-22 15:26:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,14 @@
 class Dog
-  def initialize(name)
-    @name = name
+  class Dog
+
+   def name=(dogs_name)
+     @this_dogs_name = dogs_name
+   end
+
+   def name
+     @this_dogs_name
+   end
   end
 
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-  end
-
-  fido = Dog.new("Fido")
+  fido = Dog.new
+  fido.name = "Fido"
